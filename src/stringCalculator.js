e14f266c36752fabda346124dfbfdf89658dab24
shohailahm <ahmedsu868@gmail.com>
2024-08-15 18:41:53 +0530

    condtions to add 1 or n numbers passed as args

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,3 +1,9 @@
 export function add(numbers) {
   if (numbers === "") return 0;
+
+  let delimiter = /,|\n/;
+
+  const nums = numbers.split(delimiter).map(Number);
+
+  return nums.reduce((sum, num) => sum + num, 0);
 }
--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -4,4 +4,16 @@ describe("String Calculator", () => {
   test("returns 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  test("returns the number itself when only one number is provided", () => {
+    expect(add("1")).toBe(1);
+  });
+
+  test("returns the sum of two comma-separated numbers", () => {
+    expect(add("1,2")).toBe(3);
+  });
+
+  test("handles an unknown amount of numbers", () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+  });
 });
